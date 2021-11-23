8011b41de689c7d7e5d36850d36e143315bbc6d4
Tobias Florin <tobi@casa-blu.de>
2021-11-23 21:59:55 +0100

    Revert "Memcached restart fix"
    This reverts commit 89aeac22fccd5c28f6842b98d90640508399d02f.

--- board.php
+++ board.php
@@ -195,7 +195,7 @@ if( ( (isset($Member) && $Member->status == 'Playing') || $User->id == $Game->di
 	{
 		if( isset($Member) && $Game->Members->votesPassed() && $Game->phase!='Finished' )
 		{
-			if( is_null($MC->append('processHint',','.$Game->id)) ) $MC->set('processHint', ','.$Game->id);
+			$MC->append('processHint',','.$Game->id);
 			
 			$DB->get_lock('gamemaster',1);
 
@@ -226,7 +226,7 @@ if( ( (isset($Member) && $Member->status == 'Playing') || $User->id == $Game->di
 		}
 		else if( $Game->needsProcess() )
 		{
-			if( is_null($MC->append('processHint',','.$Game->id)) ) $MC->set('processHint', ','.$Game->id);
+			$MC->append('processHint',','.$Game->id);
 		}
 		else if ( false )
 		{

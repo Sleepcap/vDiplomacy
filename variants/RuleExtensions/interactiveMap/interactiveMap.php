9746561fb836949ac20d18c462b46379f1026aeb
Tobias Florin <tobi@casa-blu.de>
2023-11-25 22:42:31 +0100

    Fixed bug in rule extension interactive map implementation

--- variants/RuleExtensions/interactiveMap/interactiveMap.php
+++ variants/RuleExtensions/interactiveMap/interactiveMap.php
@@ -13,6 +13,9 @@ class RuleExtensionsVariant_IAmap_base extends IAmap
         if(!$this->Variant->rules[RULE_CUSTOM_MAP]){
             // by default, construct the interactive map with a Classic variant instance to laod classic map data
             $classicVariant = libVariant::loadFromVariantName('Classic');
+            // The parent constructor stores the variant itself in the property $this->Variant.
+            // To still access the rules (to skip any extensions), the rules need to be copied to the injected variant object.
+            $classicVariant->rules = $Variant->rules; 
             parent::__construct($classicVariant, $mapName);
         } else {
             parent::__construct($Variant, $mapName);
